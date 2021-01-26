97daf5e2c2a178dadf76ce270240e7e49aa8dc77
Jenifer Angelo <jenmangelo@gmail.com>
2021-01-26 10:21:08 -0300

    colocando anotações

--- Python/python_security/ping_mult.py
+++ Python/python_security/ping_mult.py
@@ -1,14 +1,14 @@
-import os
-import time
+import os  #importa a biblioteca os
+import time  #importa a biblioteca time
 
-with open('hosts.txt') as file:
-    dump = file.read()
-    dump = dump.splitlines()
+with open('hosts.txt') as file:  #abre hosts.txt como arquivo
+    dump = file.read()  #criamos a variável dump que vai ler o arquivo hosts.txt
+    dump = dump.splitlines()  #coloca o conteúdo de dump em linhas separadas
 
     for ip in dump:
         print('-' * 60)
         print('Verificando o IP ', ip)
         os.system('ping -c 2 {}'.format(ip))
-        time.sleep(2)
+        time.sleep(2)  #faz uma pausa\delay entre as execuções
 
     print('-' * 60)
--- Python/python_security/ping_simples.py
+++ Python/python_security/ping_simples.py
@@ -1,5 +1,7 @@
-import os
+import os #importa a biblioteca os
 print('#' * 60)
 iphost = input('Digite o IP ou Host que você deseja testar a conexão: ')
-os.system('ping -c 6 {}'.format(iphost))
+os.system('ping -c 6 {}'.format(iphost)) #chamando o método system da biblioteca os e então usando o comando
+# desse método para executar o ping
+# o -c cria um contador que diz quantas vezes o ping será executado, nesse caso 6 vezes
 print('#' * 60)
