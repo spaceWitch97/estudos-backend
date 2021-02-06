94ec864a4dbda9f49bee0f4423a3877a57084c86
Jenifer Angelo <jenmangelo@gmail.com>
2021-02-06 00:03:50 -0300

    brigando com o visual studio

---
+++ Cursos/Microlins/cadastroClientes/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace cadastroClientes
+{
+    partial class formInicio
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbCpf = new System.Windows.Forms.TextBox();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(79, 102);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(47, 17);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(73, 136);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(206, 25);
+            this.txbNome.TabIndex = 1;
+            // 
+            // txbCpf
+            // 
+            this.txbCpf.Location = new System.Drawing.Point(67, 234);
+            this.txbCpf.Name = "txbCpf";
+            this.txbCpf.Size = new System.Drawing.Size(206, 25);
+            this.txbCpf.TabIndex = 3;
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(79, 198);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(32, 17);
+            this.lblCpf.TabIndex = 2;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Location = new System.Drawing.Point(297, 230);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(206, 25);
+            this.txbEmail.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(309, 194);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email:";
+            // 
+            // formInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txbCpf);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "formInicio";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbCpf;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
+
