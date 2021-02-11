7e57009a438a353433a27ca5ad8be85b2c93a88f
Jenifer Angelo <jenmangelo@gmail.com>
2021-02-11 00:15:25 -0300

    estudos!

---
+++ Exercícios/C/exercicios/vetores.c
@@ -0,0 +1,13 @@
+
+#include <stdio.stdio.h>
+/*
+Vetor é um conjunto de variáveis do mesmo tipo
+tipo_do_vetor nome_do_vetor[quantidade_de_posições];
+*/
+
+void main(){
+    int vetor[5] = {1, 2, 3, 4, 5};
+    float vetor1 [2];
+
+
+}
