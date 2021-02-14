817d5766852c4c77a9ff634b1e7fb7f75e0e730b
Jenifer Angelo <jenmangelo@gmail.com>
2021-02-14 17:47:15 -0300

    estudaando

---
+++ Cursos/Digital Innovation One/Javascript/js/main.js
@@ -0,0 +1,18 @@
+// DECLARAR VARIÁVEIS (não precisa dizer o timo, isso funciona dinamicamente)
+var nome = 'Bruxa Espacial'
+var num = 20
+var num2 = 3
+var num3 = '40'
+var frase = "EXISTENCE IS PAIN"
+
+//exibir um alerta na tela
+var idade = num + num2
+alert(nome+" tem "+idade+ " anos")
+alert(num + num2 + num3)
+
+//exibir texto no console
+console.log("don't forget: 42")
+console.log(frase)
+console.log(frase.replace("PAIN", "JOY")) //"replace" modifica algo na string, trocando pelo que você colocar dps
+
+
